786087a45fc594d353de05d40ad0bbd1a65af0e0
tester1 <anton.dekhtyar@weltrade.com>
2017-08-15 17:39:52 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        print("Java easy to learn!");
+    }
+
+    public static void print(String s) {
+        for (int i=0;i<4;i++)
+        System.out.println(s);
+
+    }
+}
+
